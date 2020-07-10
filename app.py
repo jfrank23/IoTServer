cc908598b6e1849e18e28df45163999b5a032e49
Jordan Franklin <jordo189@gmail.com>
2020-07-09 21:41:17 -0400

    init

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+
+def index_page_landing():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(host='192.168.56.1')
